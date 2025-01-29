d04f9af52ba3f41752d5d1640df74e85c5dd5bcf
Mamatyari700 <pandapool.1991.3@gmail.com>
2025-01-28 17:49:31 -0800

    added comment on hv.c

--- Core/Src/hv.c
+++ Core/Src/hv.c
@@ -5,14 +5,13 @@
 	void ReadHVInput(uint32_t *read_volt_HV) {
 		uint32_t adcValue = 0;
 
-		// ADC開始
-		HAL_ADC_Start(&hadc1);
+		HAL_ADC_Start(&hadc1);//start adc with adc1
 		if (HAL_ADC_PollForConversion(&hadc1, HAL_MAX_DELAY) == HAL_OK) {
-			adcValue = HAL_ADC_GetValue(&hadc1);
+			adcValue = HAL_ADC_GetValue(&hadc1);//get adc value and store it in adcValue
 		}
-		HAL_ADC_Stop(&hadc1);
+		HAL_ADC_Stop(&hadc1);//stop adc
 
-		// 電圧計算
+		//calculate voltage based on  resolution and gain on opamp, voltage divider ratio
 		float adcVoltage = ((float)adcValue / ADC_RESOLUTION) * V_REF;
 		float amcOutput = adcVoltage / GAIN_TLV9001;
 		float hvInput = amcOutput * DIVIDER_RATIO;
