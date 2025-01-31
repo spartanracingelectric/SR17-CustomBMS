33ccef49b4a91511ae188c9b4005fb3e34dfae08
Mamatyari700 <pandapool.1991.3@gmail.com>
2025-01-30 22:00:54 -0800

    sending CAN every 1 sec

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -45,7 +45,7 @@ extern "C" {
 #define NUM_THERM_TOTAL			NUM_DEVICES*NUM_THERM_PER_MOD
 #define NUM_AUX_GROUP			6
 #define NUM_AUXES				NUM_DEVICES*NUM_AUX_GROUP
-#define LTC_DELAY				10 //500ms update delay
+#define LTC_DELAY				1000 //500ms update delay
 #define LED_HEARTBEAT_DELAY_MS	10  //500ms update delay
 #define BALANCE 0 //FALSE
 /* USER CODE END Private defines */
@@ -101,16 +101,6 @@ void Error_Handler(void);
 #define MCU_HEARTBEAT_LED_GPIO_Port GPIOC
 
 /* USER CODE BEGIN Private defines */
-#define NUM_DEVICES				8	//1 slave board
-#define NUM_CELL_SERIES_GROUP	12	//1 slave board
-#define NUM_CELLS				NUM_DEVICES*NUM_CELL_SERIES_GROUP	//multiple slave board
-#define NUM_THERM_PER_MOD		12
-#define NUM_THERM_TOTAL			NUM_DEVICES*NUM_THERM_PER_MOD
-#define NUM_AUX_GROUP			6
-#define NUM_AUXES				NUM_DEVICES*NUM_AUX_GROUP
-#define LTC_DELAY				10 //500ms update delay
-#define LED_HEARTBEAT_DELAY_MS	10  //500ms update delay
-#define BALANCE 0 //FALSE
 /* USER CODE END Private defines */
 
 #ifdef __cplusplus
