f08beef57504372420a89e49dca14b7e7bade930
Mamatyari700 <pandapool.1991.3@gmail.com>
2025-02-02 18:39:56 -0800

    change the fault and warning bit to varoables, edited dbf according to that changes

--- Core/Src/safety.c
+++ Core/Src/safety.c
@@ -1,29 +1,41 @@
 #include "safety.h"
 #include "main.h"
-
+#include "stdio.h"
 // ! Fault Thresholds
 
 // Refer to TODO on Line 61
 #define PACK_HIGH_VOLT_FAULT	    4100000
 #define PACK_LOW_VOLT_FAULT         2880000
-
 #define CELL_HIGH_VOLT_FAULT	    42000
 #define CELL_LOW_VOLT_FAULT		    25000
-
+#define CELL_VOLT_IMBALANCE_FAULT   2000 //0.1 V
 #define CELL_HIGH_TEMP_FAULT		60
 
 // ! Warnings Thresholds
 #define PACK_HIGH_VOLT_WARNING	    4085000
 #define PACK_LOW_VOLT_WARNING       3000000
-
 #define CELL_HIGH_VOLT_WARNING	    40000
 #define CELL_LOW_VOLT_WARNING	    27000
-
+#define CELL_VOLT_IMBALANCE_WARNING	1000 //0.05 V
 #define CELL_HIGH_TEMP_WARNING		55
 #define CELL_LOW_TEMP_WARNING		0
 
-#define CELL_VOLT_IMBALANCE_FAULT   2000 //0.1 V
-#define CELL_VOLT_IMBALANCE_WARNING	1000 //0.05 V
+#define HYSTERESIS_MARGIN 10
+
+#define FAULT_BIT_HIGH_PACK_VOLT 	(1 << 7)	// 0b10000000 (Bit position 7)
+#define FAULT_BIT_LOW_PACK_VOLT  	(1 << 6)	// 0b01000000 (Bit position 6)
+#define FAULT_BIT_HIGH_VOLT    		(1 << 5)  	// 0b00010000 (Bit position 5)
+#define FAULT_BIT_LOW_VOLT     		(1 << 4)  	// 0b00001000 (Bit position 4)
+#define FAULT_BIT_IMBALANCE       	(1 << 3)  	// 0b00000100 (Bit position 3)
+#define FAULT_BIT_HIGH_TEMP 		(1 << 2) 	// 0b00000010 (Bit position 2)
+
+#define WARNING_BIT_HIGH_PACK_VOLT 	(1 << 7)	// 0b10000000 (Bit position 7)
+#define WARNING_BIT_LOW_PACK_VOLT  	(1 << 6)	// 0b01000000 (Bit position 6)
+#define WARNING_BIT_HIGH_VOLT    	(1 << 5)  	// 0b00010000 (Bit position 5)
+#define WARNING_BIT_LOW_VOLT     	(1 << 4)  	// 0b00001000 (Bit position 4)
+#define WARNING_BIT_IMBALANCE       (1 << 3)  	// 0b00000100 (Bit position 3)
+#define WARNING_BIT_HIGH_TEMP 		(1 << 2) 	// 0b00000010 (Bit position 2)
+
 
 void Cell_Summary_Voltage(struct batteryModule *batt, uint8_t *fault,
 		uint8_t *warnings, uint8_t *states, uint8_t *low_volt_hysteresis,
@@ -35,67 +47,68 @@ void Cell_Summary_Voltage(struct batteryModule *batt, uint8_t *fault,
 
 		if (batt->cell_volt[i] > batt->cell_volt_highest) {
 			batt->cell_volt_highest = batt->cell_volt[i];
-
-			if (batt->cell_volt_highest >= CELL_HIGH_VOLT_WARNING) {
-					*warnings |= 0b00010000;
-				}
-
-			//high cell volt fault
-				if ((batt->cell_volt_highest >= CELL_HIGH_VOLT_FAULT)
-						&& ((*high_volt_hysteresis) > 0)) {
-					*fault |= 0b00010000;
-					HAL_GPIO_WritePin(MCU_SHUTDOWN_SIGNAL_GPIO_Port, MCU_SHUTDOWN_SIGNAL_Pin, GPIO_PIN_SET);
-				} else if (batt->cell_volt_highest >= CELL_HIGH_VOLT_FAULT) {
-					*high_volt_hysteresis = 1;
-				} else {
-					*high_volt_hysteresis = 0;
-				}
-				//end of high cell volt fault
+			printf("high voltage fault: %d\n", batt->cell_volt_highest);
 		}
+
+		if (batt->cell_volt_highest >= CELL_HIGH_VOLT_WARNING && batt->cell_volt_highest < CELL_HIGH_VOLT_FAULT) {
+				*warnings |= WARNING_BIT_HIGH_VOLT;
+			}
+
+		//high cell volt fault
+		if ((batt->cell_volt_highest >= CELL_HIGH_VOLT_FAULT) /*&& ((*high_volt_hysteresis) > 0)*/) {
+			*high_volt_hysteresis = 1;
+			*fault |= FAULT_BIT_HIGH_VOLT;
+			HAL_GPIO_WritePin(MCU_SHUTDOWN_SIGNAL_GPIO_Port, MCU_SHUTDOWN_SIGNAL_Pin, GPIO_PIN_SET);
+			printf("high voltage fault signal on\n");
+		}
+		else if (batt->cell_volt_highest < CELL_HIGH_VOLT_FAULT - HYSTERESIS_MARGIN && *high_volt_hysteresis == 1){
+			*high_volt_hysteresis = 0;
+			*fault &= ~FAULT_BIT_HIGH_VOLT;
+		}
+			//end of high cell volt fault
+
 
 		if (batt->cell_volt[i] < batt->cell_volt_lowest) {
 			batt->cell_volt_lowest = batt->cell_volt[i];
-
-			if (batt->cell_volt_lowest <= CELL_LOW_VOLT_WARNING) {
-								*warnings |= 0b00100000;
-							}
-
-			//low cell volt fault
-			if ((batt->cell_volt_lowest <= CELL_LOW_VOLT_FAULT)
-					&& ((*low_volt_hysteresis) > 0)) {
-				*fault |= 0b00100000;
-				HAL_GPIO_WritePin(MCU_SHUTDOWN_SIGNAL_GPIO_Port, MCU_SHUTDOWN_SIGNAL_Pin, GPIO_PIN_SET);
-			} else if (batt->cell_volt_lowest <= CELL_LOW_VOLT_FAULT) {
-				*low_volt_hysteresis = 1;
-			} else {
-				*low_volt_hysteresis = 0;
-			}
-			//end of low cell volt fault
-
 		}
 
+		if (batt->cell_volt_lowest <= CELL_LOW_VOLT_WARNING) {
+			*warnings |= WARNING_BIT_LOW_VOLT;
+		}
+//			low cell volt fault
+		if (batt->cell_volt_lowest <= CELL_LOW_VOLT_FAULT) {
+			*low_volt_hysteresis = 1;
+			*fault |= FAULT_BIT_LOW_VOLT;
+			HAL_GPIO_WritePin(MCU_SHUTDOWN_SIGNAL_GPIO_Port, MCU_SHUTDOWN_SIGNAL_Pin, GPIO_PIN_SET);
+		}
+		else if(batt->cell_volt_lowest > CELL_LOW_VOLT_FAULT + HYSTERESIS_MARGIN && *low_volt_hysteresis == 1){
+			*low_volt_hysteresis = 0;
+			*fault &= ~FAULT_BIT_LOW_VOLT;
+		}
+//			end of low cell volt fault
+
 		//cell volt imbalance fault
-		if (((batt->cell_volt_highest - batt->cell_volt_lowest)
-				>= CELL_VOLT_IMBALANCE_FAULT) && ((*cell_imbalance_hysteresis) > 0)) {
-				*fault |= 0b00000100;
-				HAL_GPIO_WritePin(MCU_SHUTDOWN_SIGNAL_GPIO_Port, MCU_SHUTDOWN_SIGNAL_Pin, GPIO_PIN_SET);
-			} else if ((batt->cell_volt_highest - batt->cell_volt_lowest)
-					>= CELL_VOLT_IMBALANCE_FAULT) {
-				*cell_imbalance_hysteresis = 1;
-			} else {
-				*cell_imbalance_hysteresis = 0;
-			}
-			//end of cell volt imbalance fault
-
-			if ((batt->cell_volt_highest - batt->cell_volt_lowest)
-					>= CELL_VOLT_IMBALANCE_WARNING) {
-				*warnings |= 0b00000010;
-			}
-
+//		if (((batt->cell_volt_highest - batt->cell_volt_lowest)
+//				>= CELL_VOLT_IMBALANCE_FAULT) && ((*cell_imbalance_hysteresis) > 0)) {
+//				*fault |= FAULT_BIT_IMBALANCE;
+//				HAL_GPIO_WritePin(MCU_SHUTDOWN_SIGNAL_GPIO_Port, MCU_SHUTDOWN_SIGNAL_Pin, GPIO_PIN_SET);
+//			} else if ((batt->cell_volt_highest - batt->cell_volt_lowest)
+//					>= CELL_VOLT_IMBALANCE_FAULT) {
+//				*cell_imbalance_hysteresis = 1;
+//			} else {
+//				*cell_imbalance_hysteresis = 0;
+//			}
+//			//end of cell volt imbalance fault
+//
+//			if ((batt->cell_volt_highest - batt->cell_volt_lowest)
+//					>= CELL_VOLT_IMBALANCE_WARNING) {
+//				*warnings |= WARNING_BIT_IMBALANCE;
+//			}
+//
 //			if (BALANCE) {
 //				*states |= 0b10000000;
 //			}
-
+//
 //		uint32_t sum_voltage = 0; // uint32_t型に変更
 //
 //		for (int i = 0; i < NUM_CELLS; i++) {
@@ -103,36 +116,41 @@ void Cell_Summary_Voltage(struct batteryModule *batt, uint8_t *fault,
 //		}
 //
 //		if (sum_voltage - batt->pack_voltage ){
-
+//
 //		}
-		if (batt->pack_voltage >= PACK_HIGH_VOLT_WARNING) {
-			*warnings |= 0b10000000;
-		}
-		if (batt->pack_voltage <= PACK_LOW_VOLT_WARNING) {
-			*warnings |= 0b01000000;
-		}
-		if (batt->pack_voltage >= PACK_HIGH_VOLT_FAULT) {
-			*fault |= 0b10000000;
-			HAL_GPIO_WritePin(MCU_SHUTDOWN_SIGNAL_GPIO_Port, MCU_SHUTDOWN_SIGNAL_Pin, GPIO_PIN_SET);
-		}
-		if (batt->pack_voltage <= PACK_LOW_VOLT_FAULT) {
-			*fault |= 0b01000000;
-			HAL_GPIO_WritePin(MCU_SHUTDOWN_SIGNAL_GPIO_Port, MCU_SHUTDOWN_SIGNAL_Pin, GPIO_PIN_SET);
-		}
-
-
-		if ((batt->cell_volt_highest - batt->cell_volt_lowest)
-				>= CELL_VOLT_IMBALANCE_WARNING) {
-			*warnings |= 0b00000010;
-		}
+//		if (batt->pack_voltage >= PACK_HIGH_VOLT_WARNING) {
+//			*warnings |= WARNING_BIT_HIGH_PACK_VOLT;
+//		}
+//		if (batt->pack_voltage <= PACK_LOW_VOLT_WARNING) {
+//			*warnings |= WARNING_BIT_LOW_PACK_VOLT;
+//		}
+//		if (batt->pack_voltage >= PACK_HIGH_VOLT_FAULT) {
+//			*fault |= FAULT_BIT_HIGH_PACK_VOLT;
+//			HAL_GPIO_WritePin(MCU_SHUTDOWN_SIGNAL_GPIO_Port, MCU_SHUTDOWN_SIGNAL_Pin, GPIO_PIN_SET);
+//		}
+//		else{
+//			*fault &= ~FAULT_BIT_HIGH_PACK_VOLT;
+//		}
+//		if (batt->pack_voltage <= PACK_LOW_VOLT_FAULT) {
+//			*fault |= FAULT_BIT_LOW_PACK_VOLT;
+//			HAL_GPIO_WritePin(MCU_SHUTDOWN_SIGNAL_GPIO_Port, MCU_SHUTDOWN_SIGNAL_Pin, GPIO_PIN_SET);
+//		}
+//		else{
+//			*fault &= ~FAULT_BIT_LOW_PACK_VOLT;
+//		}
+//
+//
+//		if ((batt->cell_volt_highest - batt->cell_volt_lowest)
+//				>= CELL_VOLT_IMBALANCE_WARNING) {
+//			*warnings |= 0b00000010;
+//		}
 //		if (BALANCE) {
 //			*states |= 0b10000000;
 //		}
 	}
 }
 
-void Cell_Summary_Temperature(struct batteryModule *batt, uint8_t *fault,
-		uint8_t *warnings) {
+void Cell_Summary_Temperature(struct batteryModule *batt, uint8_t *fault, uint8_t *warnings) {
 	batt->cell_temp_highest = batt->cell_temp[0];
 	batt->cell_temp_lowest = batt->cell_temp[0];
 
@@ -140,30 +158,21 @@ void Cell_Summary_Temperature(struct batteryModule *batt, uint8_t *fault,
 
 		if (batt->cell_temp_highest < batt->cell_temp[i]) {
 			batt->cell_temp_highest = batt->cell_temp[i];
-			//highest cell temp warning
-			if (batt->cell_temp_highest >= CELL_HIGH_TEMP_WARNING) {
-					*warnings |= 0b00001000;
-				}
-			//highest cell temp fault
-			if (batt->cell_temp_highest >= CELL_HIGH_TEMP_FAULT) {
-					*fault |= 0b00001000;
-					HAL_GPIO_WritePin(MCU_SHUTDOWN_SIGNAL_GPIO_Port, MCU_SHUTDOWN_SIGNAL_Pin, GPIO_PIN_SET);
-				}
 		}
-
-		if (batt->cell_temp_lowest > batt->cell_temp[i]) {
-			batt->cell_temp_lowest = batt->cell_temp[i];
-
-			if (batt->cell_temp_lowest <= CELL_LOW_TEMP_WARNING) {
-					*warnings |= 0b00000100;
-				}
+		//highest cell temp warning
+		if (batt->cell_temp_highest >= CELL_HIGH_TEMP_WARNING && batt->cell_temp_highest < CELL_HIGH_TEMP_FAULT) {
+			*warnings |= WARNING_BIT_HIGH_TEMP;
+		}
+		//highest cell temp fault
+		if (batt->cell_temp_highest >= CELL_HIGH_TEMP_FAULT) {
+			*fault |= FAULT_BIT_HIGH_TEMP;
+			HAL_GPIO_WritePin(MCU_SHUTDOWN_SIGNAL_GPIO_Port, MCU_SHUTDOWN_SIGNAL_Pin, GPIO_PIN_SET);
+		}
+		else if (batt->cell_temp_highest < CELL_HIGH_TEMP_FAULT){
+			*fault &= ~FAULT_BIT_HIGH_TEMP;
 		}
 	}
 }
-
-
-
-
 
 void Module_Averages(struct batteryModule *batt) {
 
@@ -171,7 +180,7 @@ void Module_Averages(struct batteryModule *batt) {
 		uint16_t temp_sum = 0;
 
 		for (int j = 0; j < i + 12; j++) {
-			temp_sum += batt->cell_volt[i];
+			temp_sum += batt->cell_volt[j];
 		}
 
 		uint16_t average = temp_sum / NUM_CELL_SERIES_GROUP;
