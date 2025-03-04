cdfc7e516e6dda3208cce4c56a067c8a2b09802c
Mamatyari700 <pandapool.1991.3@gmail.com>
2025-03-03 22:24:55 -0800

    changed vref to 3.28

--- Core/Inc/hv.h
+++ Core/Inc/hv.h
@@ -3,8 +3,6 @@
 
 #include "main.h"
 
-#define ADC_RESOLUTION 4096.0f  // 12-bit ADC
-#define V_REF 3.3f              // Reference voltage (V)
 #define GAIN_TLV9001 1.58f      // TLV9001 gain
 // Resistor values for the voltage divider
 #define R1 1400060.0f  // 1.4 MΩ
