a8cecc82f9d2bc8b06941f95438e3ba703c34ae4
BenVN123 <bnguyen123.vn@gmail.com>
2025-04-02 20:00:45 -0700

    Fix SOC Pin

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -45,7 +45,7 @@ extern "C" {
 #define NUM_THERM_TOTAL			NUM_DEVICES*NUM_THERM_PER_MOD
 #define NUM_AUX_GROUP			6
 #define NUM_AUXES				NUM_DEVICES*NUM_AUX_GROUP
-#define CYCLETIME_CAP			100 //100ms update delay
+#define CYCLETIME_CAP			1000 //100ms update delay
 #define LED_HEARTBEAT_DELAY_MS	50  //10ms update delay
 #define BALANCE 				0 	//FALSE
 #define MAX_CELL_CAPACITY 		3000
@@ -115,41 +115,8 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define Shunt_PIN_FOR_2A_Pin GPIO_PIN_2
-#define Shunt_PIN_FOR_2A_GPIO_Port GPIOC
-#define LTC_nCS_Pin GPIO_PIN_4
-#define LTC_nCS_GPIO_Port GPIOA
-#define MCU_ADC_VSENSE_Pin GPIO_PIN_5
-#define MCU_ADC_VSENSE_GPIO_Port GPIOC
-#define MCU_SHUTDOWN_SIGNAL_Pin GPIO_PIN_1
-#define MCU_SHUTDOWN_SIGNAL_GPIO_Port GPIOB
-#define MCU_HEARTBEAT_LED_Pin GPIO_PIN_6
-#define MCU_HEARTBEAT_LED_GPIO_Port GPIOC
 
-/* USER CODE BEGIN Private defines */
-#define NUM_DEVICES				8	//1 slave board
-#define NUM_CELL_SERIES_GROUP	12	//1 slave board
-#define NUM_CELLS				NUM_DEVICES*NUM_CELL_SERIES_GROUP	//multiple slave board
-#define NUM_THERM_PER_MOD		12
-#define NUM_THERM_TOTAL			NUM_DEVICES*NUM_THERM_PER_MOD
-#define NUM_AUX_GROUP			6
-#define NUM_AUXES				NUM_DEVICES*NUM_AUX_GROUP
-#define CYCLETIME_CAP			100 //100ms update delay
-#define LED_HEARTBEAT_DELAY_MS	50  //10ms update delay
-#define BALANCE 				0 	//FALSE
-#define MAX_CELL_CAPACITY 		3000
-#define MAX_BATTERY_CAPACITY 	NUM_DEVICES* MAX_CELL_CAPACITY
 
-#define SHUNT_SIGNAL_Pin GPIO_PIN_3
-#define SHUNT_SIGNAL_GPIO_Port GPIOC
-#define LTC_nCS_Pin GPIO_PIN_4
-#define LTC_nCS_GPIO_Port GPIOA
-#define MCU_ADC_VSENSE_Pin GPIO_PIN_5
-#define MCU_ADC_VSENSE_GPIO_Port GPIOC
-#define MCU_SHUTDOWN_SIGNAL_Pin GPIO_PIN_1
-#define MCU_SHUTDOWN_SIGNAL_GPIO_Port GPIOB
-#define MCU_HEARTBEAT_LED_Pin GPIO_PIN_6
-#define MCU_HEARTBEAT_LED_GPIO_Port GPIOC
 /* USER CODE END Private defines */
 
 #ifdef __cplusplus
