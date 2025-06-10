1f6284ffe4d0f0139774747c13b2ec180b2f54c8
Mamatyari700 <pandapool.1991.3@gmail.com>
2025-06-09 23:45:14 -0700

    commented out the printf()

--- Core/Src/soc.c
+++ Core/Src/soc.c
@@ -31,7 +31,7 @@ void SOC_getInitialCharge(batteryModule *batt) {
         temperature += batt->cell_temp[i];
     }
     temperature /= NUM_THERM_TOTAL;
-    printf("temp:%d", temperature);
+//    printf("temp:%d", temperature);
 
     int tempCharts[] = {0, 25, 40};
 
@@ -55,7 +55,7 @@ void SOC_getInitialCharge(batteryModule *batt) {
             batt->soc = SOC_getChargeData40C((uint16_t) voltage) * NUM_DEVICES * 1000;
             break;
     }
-    printf("this is running");
+//    printf("this is running");
 }
 
 void SOC_updateCurrent(batteryModule *batt) {
